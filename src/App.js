e8b540904f55c1f05e0122994b6d1793ca47747a
manu <manu.bellemans@gmail.com>
2021-07-21 00:39:09 +0200

    URL params

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesList from "./pages/ArticlesList";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path={"/"} component={HomePage} exact />
+        <NavBar />
+        <div id={"page-body"}>
+          <Route path={"/"} component={HomePage} exact />
+          <Route path={"/about"} component={AboutPage} />
+          <Route path={"/articles-list"} component={ArticlesList} />
+          <Route path={"/article/:name"} component={ArticlePage} exact />
+        </div>
       </div>
     </Router>
   );
