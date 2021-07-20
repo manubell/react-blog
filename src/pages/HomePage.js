0b51f106b2811a303e11c329c0c1042fce9b4731
manu <manu.bellemans@gmail.com>
2021-07-21 00:17:43 +0200

    add prettier + basic page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const HomePage = () => (
+  <>
+    <h1>hallo, welcome to my blog</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Cras adipiscing enim
+      eu turpis. Ornare massa eget egestas purus. Amet tellus cras adipiscing
+      enim eu turpis egestas pretium. Neque sodales ut etiam sit amet nisl
+      purus. Amet nisl suscipit adipiscing bibendum est ultricies integer quis.
+      Nisl purus in mollis nunc sed id semper. Facilisi nullam vehicula ipsum a.
+      Ullamcorper malesuada proin libero nunc consequat interdum varius. Lorem
+      dolor sed viverra ipsum nunc. Diam vulputate ut pharetra sit amet aliquam.
+      Vehicula ipsum a arcu cursus vitae congue mauris rhoncus aenean. Magnis
+      dis parturient montes nascetur ridiculus mus mauris vitae. Lectus quam id
+      leo in vitae turpis massa sed elementum. Sit amet consectetur adipiscing
+      elit duis tristique sollicitudin nibh. Mattis pellentesque id nibh tortor
+      id aliquet lectus proin nibh.
+    </p>
+
+    <p>
+      Dignissim convallis aenean et tortor at. Sed turpis tincidunt id aliquet
+      risus feugiat in ante. Elementum curabitur vitae nunc sed velit dignissim
+      sodales ut. Eu mi bibendum neque egestas congue. Egestas quis ipsum
+      suspendisse ultrices gravida. Libero enim sed faucibus turpis in eu.
+      Venenatis a condimentum vitae sapien pellentesque habitant morbi
+      tristique. Neque ornare aenean euismod elementum nisi quis. Pretium lectus
+      quam id leo in vitae turpis. Enim lobortis scelerisque fermentum dui
+      faucibus in ornare. Non diam phasellus vestibulum lorem sed risus
+      ultricies tristique nulla. Id diam maecenas ultricies mi eget. Adipiscing
+      at in tellus integer feugiat scelerisque varius morbi. Amet consectetur
+      adipiscing elit ut aliquam purus sit amet luctus. Adipiscing commodo elit
+      at imperdiet dui. Facilisi morbi tempus iaculis urna id volutpat lacus.
+      Tellus mauris a diam maecenas sed. Malesuada proin libero nunc consequat
+      interdum.
+    </p>
+
+    <p>
+      Et magnis dis parturient montes nascetur. Morbi non arcu risus quis varius
+      quam. Lacus suspendisse faucibus interdum posuere lorem. Massa id neque
+      aliquam vestibulum morbi blandit. Condimentum lacinia quis vel eros donec
+      ac. Eu sem integer vitae justo. Euismod quis viverra nibh cras pulvinar
+      mattis. Nisi est sit amet facilisis magna etiam tempor orci eu.
+      Pellentesque sit amet porttitor eget dolor morbi. Sit amet nulla facilisi
+      morbi tempus. Felis bibendum ut tristique et egestas quis. Praesent
+      tristique magna sit amet purus gravida quis blandit turpis. Pretium lectus
+      quam id leo in vitae turpis massa.
+    </p>
+  </>
+);
+
+export default HomePage;
