e0c1cab0f8fe38e777826de940f3c8080d487fc9
manu <manu.bellemans@gmail.com>
2021-07-21 19:55:51 +0200

    ArticlesList reusable component

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import articleContent from "./article-content";
 
-const ArticlesList = () => (
+const ArticlesList = ({ articles }) => (
   <>
-    <h1>Articles</h1>
-    {articleContent.map((article) => (
+    {articles.map((article) => (
       <Link className={"article-list-item"} to={`/article/${article.name}`}>
         <h3>{article.title}</h3>
         <p>{article.content[0].substring(0, 150)}...</p>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import articleContent from "./article-content";
+import ArticlesList from "../components/ArticlesList";
 
 const ArticlePage = ({ match }) => {
   const name = match.params.name;
@@ -7,12 +8,18 @@ const ArticlePage = ({ match }) => {
 
   if (!article) return <h1>Article doesn't exist</h1>;
 
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  );
+
   return (
     <>
       <h1>This is the {article.title} article</h1>
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <h3>Other articles</h3>
+      <ArticlesList articles={otherArticles} />
     </>
   );
 };
