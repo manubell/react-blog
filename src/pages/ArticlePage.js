d6db07cd0c6934688e76b25b7bd4affd7cc5cf2a
manu <manu.bellemans@gmail.com>
2021-07-26 23:14:58 +0200

    prettier

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import ArticlesList from '../components/ArticlesList';
-import NotFoundPage from './NotFoundPage';
-import articleContent from './article-content';
+import React, { useState } from "react";
+import ArticlesList from "../components/ArticlesList";
+import NotFoundPage from "./NotFoundPage";
+import articleContent from "./article-content";
 
 const ArticlePage = ({ match }) => {
-    const name = match.params.name;
-    const article = articleContent.find(article => article.name === name);
+  const name = match.params.name;
+  const article = articleContent.find((article) => article.name === name);
 
-    if (!article) return <NotFoundPage />
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
 
-    const otherArticles = articleContent.filter(article => article.name !== name);
+  useEffect(() => {
+    return () => {
+      console.log("test");
+    };
+  }, [input]);
 
-    return (
-        <>
-        <h1>{article.title}</h1>
-        {article.content.map((paragraph, key) => (
-            <p key={key}>{paragraph}</p>
-        ))}
-        <h3>Other Articles:</h3>
-        <ArticlesList articles={otherArticles} />
-        </>
-    );
-}
+  if (!article) return <NotFoundPage />;
+
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  );
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
+      <h3>Other Articles:</h3>
+      <ArticlesList articles={otherArticles} />
+    </>
+  );
+};
 
 export default ArticlePage;
